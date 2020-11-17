846a018c8767c4ce6fa44803637330e2c61a5b6d
maxl95 <maxl.fischer@gmx.de>
2020-11-17 21:31:08 +0100

    update layout file

---
+++ XGerber/OSHPark_rename.bat
@@ -0,0 +1,17 @@
+rename *.Outline *.GKO
+rename *.Drill *.XLN
+rename *.PosiTop *.GTO
+rename *.StopTop *.GTS
+rename *.Top *.GTL
+rename *.Bot *.GBL
+rename *.StopBot *.GBS
+rename *.PosiBot *.GBO
+mkdir OSHPark_export
+move *.GKO OSHPark_export/
+move *.XLN OSHPark_export/
+move *.GTO OSHPark_export/
+move *.GTS OSHPark_export/
+move *.GTL OSHPark_export/
+move *.GBL OSHPark_export/
+move *.GBS OSHPark_export/
+move *.GBO OSHPark_export/
